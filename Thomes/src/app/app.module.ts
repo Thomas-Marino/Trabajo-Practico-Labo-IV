98416decaab619783ebe9d0f64d1600c773a92a6
Thomas-Marino <thomasmarino627@gmail.com>
2024-09-08 18:08:02 -0300

    Implementaciones de los componentes creados

--- Thomes/src/app/app.module.ts
+++ Thomes/src/app/app.module.ts
@@ -5,10 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AngularFireModule } from '@angular/fire/compat';
 import { environment } from '../environments/environment';
+import { IngresoComponent } from './components/ingreso/ingreso.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { HeaderComponent } from './components/header/header.component';
+import { QuienSoyComponent } from './components/quien-soy/quien-soy.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IngresoComponent,
+    InicioComponent,
+    HeaderComponent,
+    QuienSoyComponent
   ],
   imports: [BrowserModule, AppRoutingModule, AngularFireModule.initializeApp(environment.firebaseConfig)],
   providers: [],
