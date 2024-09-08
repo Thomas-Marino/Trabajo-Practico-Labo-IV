857aa6fba1de98e6aef1f79cb8f71e0c321ccfc6
Thomas-Marino <thomasmarino627@gmail.com>
2024-09-08 15:52:21 -0300

    Implementacion de angular/fire y firebase hosting

---
+++ Thomes/src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+    firebaseConfig: {
+        apiKey: "AIzaSyBC-B5pJ-Tg4YO6FFX7UGK3V66w5mji0AI",
+        authDomain: "lab-iv-thomes.firebaseapp.com",
+        projectId: "lab-iv-thomes",
+        storageBucket: "lab-iv-thomes.appspot.com",
+        messagingSenderId: "121295997910",
+        appId: "1:121295997910:web:e45acc0d875a9fa77f3277"
+    }
+};
