6cc63ea716557ea6661d66a0e4ce53c7dbc51dbc
Thomas-Marino <thomasmarino627@gmail.com>
2024-09-08 18:07:13 -0300

    Routeo implementado

--- Thomes/src/app/app-routing.module.ts
+++ Thomes/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { IngresoComponent } from './components/ingreso/ingreso.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { QuienSoyComponent } from './components/quien-soy/quien-soy.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/ingreso', pathMatch: 'full'},
+  {path: 'ingreso', component: IngresoComponent},
+  {path: 'home', component: InicioComponent},
+  {path: 'quien-soy', component: QuienSoyComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
