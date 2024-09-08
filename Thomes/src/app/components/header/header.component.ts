bfb82fa8da1e78f984525306333f19deeead417f
Thomas-Marino <thomasmarino627@gmail.com>
2024-09-08 19:29:26 -0300

    Implementacion de nombre del usuario y cierre de sesion

--- Thomes/src/app/components/header/header.component.ts
+++ Thomes/src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterService } from '../../services/router.service';
 import { AuthService } from '../../services/firebase-auth.service';
@@ -9,9 +9,22 @@ import { SwalService } from '../../services/swal.service';
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent
+export class HeaderComponent implements OnInit
 {
 	constructor(public router:RouterService, public authService:AuthService, public swalService:SwalService) {}
+
+  nombreUsuario: string = "";
+
+  ngOnInit(): void 
+  {
+    this.ObtenerUsuario();
+    console.log(this.authService.ObtenerUsuario())
+  }
+
+  async ObtenerUsuario() // Creo una función obtener usuario nuevamente para que oninit no sea async.
+  {
+    this.nombreUsuario = await this.authService.ObtenerUsuario();
+  }
 
   async CerrarSesion():Promise<void>
 	{
