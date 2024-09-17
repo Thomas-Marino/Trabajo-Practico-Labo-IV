ec714aeb263c45393c6f9d7bea1d11c14e23c934
Thomas-Marino <thomasmarino627@gmail.com>
2024-09-17 17:14:58 -0300

    Push 2do sprint

--- Thomes/src/app/app.module.ts
+++ Thomes/src/app/app.module.ts
@@ -1,17 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+// ------------ Components -----------
 import { AppComponent } from './app.component';
-import { AngularFireModule } from '@angular/fire/compat';
-import { environment } from '../environments/environment';
-import { IngresoComponent } from './components/ingreso/ingreso.component';
-import { InicioComponent } from './components/inicio/inicio.component';
 import { HeaderComponent } from './components/header/header.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { IngresoComponent } from './components/ingreso/ingreso.component';
 import { QuienSoyComponent } from './components/quien-soy/quien-soy.component';
-import { FormsModule } from '@angular/forms';
-import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+// ------------ Modulos -----------
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+// ---- Material
+import { AngularMaterialModule } from './modules/angular-material/angular-material.module'
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+// ---- FireBase
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore'
+// ---- Variables de entorno
+import { environment } from '../environments/environment';
+import { RegistroComponent } from './components/registro/registro.component';
 
 @NgModule({
   declarations: [
@@ -19,10 +27,20 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
     IngresoComponent,
     InicioComponent,
     HeaderComponent,
-    QuienSoyComponent
+    QuienSoyComponent,
+    RegistroComponent
   ],
-  imports: [BrowserModule, AppRoutingModule, FormsModule, AngularFireModule.initializeApp(environment.firebaseConfig), AngularFireAuthModule, NgbModule],
-  providers: [],
+  imports: [
+    BrowserModule, 
+    AppRoutingModule, 
+    FormsModule, 
+    AngularFireModule.initializeApp(environment.firebaseConfig), 
+    AngularFireAuthModule, 
+    AngularFirestoreModule, 
+    NgbModule, 
+    AngularMaterialModule
+  ],
+  providers: [provideAnimationsAsync()],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
