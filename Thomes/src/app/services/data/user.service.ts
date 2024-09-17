a75bdd19a2abf8a2caeed1089dd1a76582033fbc
Thomas-Marino <thomasmarino627@gmail.com>
2024-09-17 17:12:34 -0300

    Implementacion de servicio para la comunicacion entre componentes

---
+++ Thomes/src/app/services/data/user.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { AuthService } from '../firebase/firebase-auth.service'
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService 
+{
+  constructor(public authService:AuthService) { }
+
+  // BehaviorSubject para emitir el nombre del usuario
+  private nombreUsuarioSubject = new BehaviorSubject<string>('');
+  nombreUsuario$ = this.nombreUsuarioSubject.asObservable();
+
+  AsignarNombreUsuario(nombre: string): void 
+  { 
+    this.nombreUsuarioSubject.next(nombre); 
+  }
+
+  // Función para obtener el valor actual del usuario (sin necesidad de suscribirse)
+  ObtenerNombreUsuario(): string 
+  { 
+    return this.nombreUsuarioSubject.getValue(); 
+  }
+}
