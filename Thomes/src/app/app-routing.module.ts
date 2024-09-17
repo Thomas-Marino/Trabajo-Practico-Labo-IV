6ebaf41daff8a25d20b8620f4c8efbdf70c3bee3
Thomas-Marino <thomasmarino627@gmail.com>
2024-09-17 17:13:45 -0300

    Implementacion de ruta registro + home como ruta deffault

--- Thomes/src/app/app-routing.module.ts
+++ Thomes/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { IngresoComponent } from './components/ingreso/ingreso.component';
 import { InicioComponent } from './components/inicio/inicio.component';
 import { QuienSoyComponent } from './components/quien-soy/quien-soy.component';
+import { RegistroComponent } from './components/registro/registro.component';
 
 const routes: Routes = [
-  {path: '', redirectTo: '/ingreso', pathMatch: 'full'},
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
   {path: 'ingreso', component: IngresoComponent},
   {path: 'home', component: InicioComponent},
+  {path: 'registro', component: RegistroComponent},
   {path: 'quien-soy', component: QuienSoyComponent}];
 
 @NgModule({
