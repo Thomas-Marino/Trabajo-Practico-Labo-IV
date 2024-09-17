38e12c9d799f69d432c4a5a799ace8f5d03bbd01
Thomas-Marino <thomasmarino627@gmail.com>
2024-09-17 17:11:52 -0300

    Implementacion de modulos + angular material module

---
+++ Thomes/src/app/modules/angular-material/angular-material.module.ts
@@ -0,0 +1,77 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+// ---------------- Material ------------------//
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCardModule } from '@angular/material/card';
+import { MatSelectModule } from '@angular/material/select';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatTableModule } from '@angular/material/table';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatHint } from '@angular/material/form-field';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MatDialogModule,
+    MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatCardModule,
+    MatSelectModule,
+    MatCheckboxModule,
+    MatMenuModule,
+    MatIconModule,
+    MatTableModule,
+    MatExpansionModule,
+    MatDividerModule,
+    MatSidenavModule,
+    MatSlideToggleModule,
+    MatPaginatorModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatAutocompleteModule,
+    MatChipsModule,
+    MatToolbarModule,
+    MatHint
+  ],
+  exports: [
+    CommonModule,
+    MatDialogModule,
+    MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatCardModule,
+    MatSelectModule,
+    MatCheckboxModule,
+    MatMenuModule,
+    MatIconModule,
+    MatTableModule,
+    MatExpansionModule,
+    MatDividerModule,
+    MatSidenavModule,
+    MatSlideToggleModule,
+    MatPaginatorModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatAutocompleteModule,
+    MatChipsModule,
+    MatToolbarModule,
+    MatHint
+  ]
+})
+export class AngularMaterialModule { }
