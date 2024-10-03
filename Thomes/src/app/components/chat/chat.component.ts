3c387ccb1eef186d20ce9a3cd26809537a52c5d1
Thomas-Marino <thomasmarino627@gmail.com>
2024-10-03 05:06:28 -0300

    Cambios de estilos

--- Thomes/src/app/components/chat/chat.component.ts
+++ Thomes/src/app/components/chat/chat.component.ts
@@ -1,28 +1,35 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { Observable } from 'rxjs';
 import { ChatService, IMensaje } from '../../services/chat.service';
 import { FirebaseStoreService } from '../../services/firebase/firebase-store.service';
+import { AuthService } from '../../services/firebase/firebase-auth.service';
 
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
   styleUrl: './chat.component.scss'
 })
-export class ChatComponent {
+export class ChatComponent implements OnInit{
   fireStoreService = inject(FirebaseStoreService);
+  fireAuthService = inject(AuthService);
   chatService = inject(ChatService);
   
 
   observableMensajes: Observable<IMensaje[]>;
   mensaje: string = '';
   chatAbierto: boolean = false;
+  usuarioLogueado: string = "";
 	// this.chat.observableNombreUsuario.subscribe(nombre => { this.nombreUsuario = nombre; });
 
 
   constructor() 
   {
     this.observableMensajes = this.chatService.ObtenerMensajes();
+  }
+
+  async ngOnInit(): Promise<void> {
+    this.usuarioLogueado = await this.fireAuthService.ObtenerUsuario();
   }
 
   AlternarChat(): void 
