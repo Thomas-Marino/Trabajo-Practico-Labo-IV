06dff58b6d7c62e74108c991538571553d40bdb4
Thomas-Marino <thomasmarino627@gmail.com>
2024-10-10 13:52:54 -0300

    Modificaciones realizadas

--- Thomes/src/app/services/firebase/firebase-store.service.ts
+++ Thomes/src/app/services/firebase/firebase-store.service.ts
@@ -1,32 +1,29 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FirebaseStoreService {
+  firestore = inject(AngularFirestore);
+  
+  constructor() {}
 
-  constructor(public store:AngularFirestore) { }
-
-  GuardarIngresoUsuario(nombreUsuario:string) : void
+  GuardarIngresoUsuario(nombreUsuario:string): void
   {
-    const col = this.store.collection("ingresos-usuarios");
+    const col = this.firestore.collection("ingresos-usuarios");
 
     const objetoUsuario = { usuario: nombreUsuario, fechaIngreso: new Date() };
-
     col.add(objetoUsuario);
   }
 
-  GuardarContenido(coleccion: string, datos: any)
+  GuardarContenido(coleccion: string, datos: any): void
   {
-    const col = this.store.collection(coleccion);
+    const col = this.firestore.collection(coleccion);
 
     if(datos.length > 0)
     {
-      for (const dato of datos) 
-      {
-        col.add(dato);  
-      }
+      for (const dato of datos) { col.add(dato); }
     }
     else { col.add(datos); }
   }
